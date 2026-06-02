c0f8c1e56984f68eb23389757f7e793df7e77519
park1541 <a059771@gmail.com>
2026-06-02 15:12:07 +0900

    project create

---
+++ src/main/java/com/example/backend_novel_review/BackendNovelReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_novel_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNovelReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNovelReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_novel_review/BackendNovelReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_novel_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNovelReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
